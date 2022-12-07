aa66c06e8f8a4150d7296196e12ddad960efcd18
sjohnsonn <sjohnson24@scienceleasdersjip.org>
2022-12-07 09:21:21 -0500

    Described the song information and fixed describe self.

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,9 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
- new musicVideo("Baby Shark Dance", 11.74f, "June 17, 2016", 1, "Pinkfong Baby Shark - Kids' Songs & Stories");
+        new musicVideo("Baby Shark Dance", 11.74f, "June 17, 2016", 1, "Pinkfong Baby Shark - Kids' Songs & Stories");
+
+        new song("White Christmas",50000000,"1942","Bing Crosby");
         for (music data : music.getAllData()) {
             data.describeSelf();
         }
--- src/song.java
+++ src/song.java
@@ -1,12 +1,12 @@
 public class song extends music {
 
     private String Artist;
-    private int sales;
 
-    public song(String name, float interactions, String released, String artist, int sales) {
+
+    public song(String name, float interactions, String released, String artist) {
         super(name, interactions, released);
         Artist = artist;
-        this.sales = sales;
+
     }
 
     public String getArtist() {
@@ -17,11 +17,8 @@ public class song extends music {
         Artist = artist;
     }
 
-    public int getSales() {
-        return sales;
-    }
 
-    public void setSales(int sales) {
-        this.sales = sales;
+    void describeSelf() {
+        System.out.println(getName() + " was created by " + getArtist() + ". It was released in the year " + getReleased() + ". It had " + getInteractions() + "views." );
     }
 }
